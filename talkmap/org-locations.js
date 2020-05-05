9ab7289ae32f0e2c2d394c20d4f3a82894e99d10
Tessa Davey <tessadavey@outlook.com>
2020-05-05 19:29:42 +0900

    Talks map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,117 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Regensburg, Germany",
+    49.0195333,
+    12.0974869
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Salt Lake City, UT, USA",
+    40.7596198,
+    -111.8867975
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Pittsburgh, PA, USA",
+    40.4416941,
+    -79.9900861
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Oxford, UK",
+    51.7520131,
+    -1.2578499
+  ],
+  [
+    "Orlando, FL, USA",
+    28.5421109,
+    -81.3790304
+  ],
+  [
+    "Daytona Beach, FL, USA",
+    29.2108147,
+    -81.0228331
+  ],
+  [
+    "San Diego, CA, USA",
+    32.7174209,
+    -117.1627714
+  ],
+  [
+    "Tohoku University, Japan",
+    38.2530945,
+    140.8736593
+  ],
+  [
+    "Tokyo Denki University, Tokyo, Japan",
+    35.747853649999996,
+    139.80661884465735
+  ],
+  [
+    "Saint-Malo, France",
+    48.6454528,
+    -2.015418
+  ],
+  [
+    "Yokohama, Japan",
+    35.444991,
+    139.636768
+  ],
+  [
+    "Queretaro, Mexico",
+    20.8542575,
+    -99.84756
+  ],
+  [
+    "Marina Bay Sands, Singapore",
+    1.2836965,
+    103.8607226
+  ],
+  [
+    "Imperial College London, London, UK",
+    51.49887085,
+    -0.17560795583940397
+  ],
+  [
+    "University of Tokyo, Japan",
+    35.9019845,
+    139.9362195454431
+  ],
+  [
+    "Okinawa, Japan",
+    26.5707754,
+    128.0255901
+  ],
+  [
+    "Loano, Italy",
+    44.1287537,
+    8.2593713
+  ],
+  [
+    "Santos, Brazil",
+    -23.960833,
+    -46.333889
+  ],
+  [
+    "Singapore",
+    1.357107,
+    103.8194992
+  ],
+  [
+    "Tokyo, Japan",
+    35.6828387,
+    139.7594549
+  ],
+  [
+    "Awaji Island, Japan",
+    34.3653794,
+    134.8261495
+  ],
+  [
+    "Gold Coast, Australia",
+    -28.0023731,
+    153.4145987
+  ],
+  [
+    "Kingston University, London, UK",
+    51.4034848,
+    -0.3044737
   ]
 ];
